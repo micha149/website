4843f3ca194423ac0791c1da6e08748d3dcd6790
Michael van Engelshoven <michael@van-engelshoven.de>
2022-01-25 20:12:36 +0100

    Add hero section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,22 @@
 import React from 'react';
 import { Helmet } from "react-helmet"
-import mainImage from '../images/main.png';
+import Hero from '../components/Hero';
+import { Image as Writing } from '../images/micha149-writing.svg';
 
 const Home = () => (
-    <div className="min-h-screen bg-black flex items-center justify-center">
+    <>
         <Helmet htmlAttributes={{lang: 'de'}}>
             <title>Michael van Engelshoven</title>
             <meta name="description" content="Homepage von Michael van Engelshoven, Fullstack Developer" />
         </Helmet>
 
-        <img src={mainImage} alt="Stilistischer Schriftzug: Michael van Engelshoven, coming soon" />
-    </div>
+        <Hero className="min-h-screen">
+            <h1>
+                <span id="pageTitle" className="sr-only">micha149's website</span>
+                <Writing aria-describedby="pageTitle" className="w-64 -rotate-6 dark:text-slate-200"/>
+            </h1>
+        </Hero>
+    </>
 );
 
 export default Home;
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,19 @@ module.exports = {
         './src/components/*.{tsx, ts}',
     ],
     theme: {
-        extend: {},
+        extend: {
+            animation: {
+                hero: 'hero 20s infinite ease-in-out',
+            },
+            keyframes: {
+                hero: {
+                    '0%, 100%': { transform: 'scale(1) translate(20px, 50px)' },
+                    '40%': { transform: 'scale(1.5) translate(-20px, 50px)' },
+                    '60%': { transform: 'scale(1.1) translate(10px, -50px)' },
+                    '80%': { transform: 'scale(1.3) translate(-10px, 20px)' },
+                }
+            }
+        },
     },
     plugins: [],
 };
