d8f61b2880293676a5a8584aa44247370817f9d8
Michael van Engelshoven <michael@van-engelshoven.de>
2022-02-13 15:08:19 +0100

    Add scroll indicator
    Visitors not always notice that there is more content than just the hero section.
    A scroll indicator will motivate to scroll down. The indicator disappears when the
    about section enter the visible area. To ensure a gentle fade out, the animation
    is paused when the ireation ends, where the arrow is not visible anyway. Because
    the about section is also not intersecting the viewport when scolling further down
    I also need to check if the hero section is visible, this ensures a proper behavior
    when scrolling back up speedy.

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,8 +11,21 @@ module.exports = {
             animation: {
                 hero: 'hero 20s infinite ease-in-out',
                 heartbeat: 'heartbeat 3s infinite ease-in-out',
+                scroll: 'down 3s infinite linear, appear 3s infinite ease-in-out',
             },
             keyframes: {
+                appear: {
+                    '0%': { opacity: 0 },
+                    '20%': { opacity: 0 },
+                    '45%': { opacity: 1 },
+                    '55%': { opacity: 1 },
+                    '80%': { opacity: 0 },
+                    '100%': { opacity: 0 },
+                },
+                down: {
+                    '0%': { transform: 'translate(0, -100%)'},
+                    '100%': { transform: 'translate(0, 100%)'},
+                },
                 hero: {
                     '0%, 100%': { transform: 'scale(1) translate(20px, 50px)' },
                     '40%': { transform: 'scale(1.5) translate(-20px, 50px)' },
