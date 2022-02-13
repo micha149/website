f0b9443196ac2dbd1f21af97e70e379b78699922
Michael van Engelshoven <michael@van-engelshoven.de>
2022-02-13 14:43:48 +0100

    Add page layout with footer
    Footer and page wrapping padding is placed in a Layout component
    to be reused accross diffrent pages. I am not using `gatsby-plugin-layout`
    or gatsbys `wrapPageElement` browser API to keep things simple. I may use
    one of them in the future if I want to implement page transitions.

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,7 @@ module.exports = {
         extend: {
             animation: {
                 hero: 'hero 20s infinite ease-in-out',
+                heartbeat: 'heartbeat 3s infinite ease-in-out',
             },
             keyframes: {
                 hero: {
@@ -17,7 +18,14 @@ module.exports = {
                     '40%': { transform: 'scale(1.5) translate(-20px, 50px)' },
                     '60%': { transform: 'scale(1.1) translate(10px, -50px)' },
                     '80%': { transform: 'scale(1.3) translate(-10px, 20px)' },
-                }
+                },
+                heartbeat: {
+                    '0%, 100%': { transform: 'scale(1)' },
+                    '7%': { transform: 'scale(1.25)' },
+                    '14%': { transform: 'scale(0.95)' },
+                    '21%': { transform: 'scale(1.15)' },
+                    '28%': { transform: 'scale(1)' },
+                },
             }
         },
     },
